72a47b43ae914e4ab3ef7b8b302970380fc91d30
Tommy <tommykindle83@gmail.com>
2020-04-14 03:01:57 -0700

    added fontawsome styled trashcan icon for delete

--- vuextodo/src/components/Todos.vue
+++ vuextodo/src/components/Todos.vue
@@ -2,7 +2,10 @@
   <div>
     <h3>Todos</h3>
     <div class="todos">
-      <div v-for="todo in allTodos" v-bind:key="todo.id" class="todo">{{todo.title}}</div>
+      <div v-for="todo in allTodos" v-bind:key="todo.id" class="todo">
+        {{todo.title}}
+        <i class="fas fa-trash-alt"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -34,6 +37,13 @@ export default {
   border-radius: 5px;
   text-align: center;
   position: relative;
+  cursor: pointer;
+}
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
   cursor: pointer;
 }
 </style>
